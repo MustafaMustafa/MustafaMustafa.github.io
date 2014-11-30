92095b1f1aabfdcee9159b814d3d5a5954f79ba1
Mustafa Mustafa <mstftsm@gmail.com>
2014-11-30 10:51:53 -0800

    script to get latest cv version from github

---
+++ resume/publish_latest_cv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget --output-document cv.html https://raw.githubusercontent.com/MustafaMustafa/curriculum_vitae/master/index.html
+wget --output-document mustafa_mustafa_cv.pdf https://github.com/MustafaMustafa/curriculum_vitae/raw/master/mustafa_mustafa_cv.pdf
