d3da47bdf64f368c3c7ed7b19670de85a2bc6121
Mustafa Mustafa <mstftsm@gmail.com>
2014-12-11 13:38:20 -0800

    update to automatically commit and push new CV

--- resume/publish_latest_cv.sh
+++ resume/publish_latest_cv.sh
@@ -2,3 +2,7 @@
 wget --output-document style.css https://raw.githubusercontent.com/MustafaMustafa/curriculum_vitae/master/style.css
 wget --output-document cv.html https://raw.githubusercontent.com/MustafaMustafa/curriculum_vitae/master/index.html
 wget --output-document mustafa_mustafa_cv.pdf https://github.com/MustafaMustafa/curriculum_vitae/raw/master/mustafa_mustafa_cv.pdf
+
+git add --all . 
+git commit -m "publish latest CV"
+git push
