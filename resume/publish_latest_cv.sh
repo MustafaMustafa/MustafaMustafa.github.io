ee14fd3b1b14888764446c97fb4bfeb3af6e4b45
Mustafa Mustafa <mstftsm@gmail.com>
2015-02-01 21:53:31 -0800

    publish latest CV

--- resume/publish_latest_cv.sh
+++ resume/publish_latest_cv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 wget --output-document style.css https://raw.githubusercontent.com/MustafaMustafa/curriculum_vitae/master/style.css
 wget --output-document cv.html https://raw.githubusercontent.com/MustafaMustafa/curriculum_vitae/master/index.html
+wget --output-document resume.html https://raw.githubusercontent.com/MustafaMustafa/curriculum_vitae/master/index_resume.html
 wget --output-document mustafa_mustafa_cv.pdf https://github.com/MustafaMustafa/curriculum_vitae/raw/master/mustafa_mustafa_cv.pdf
+wget --output-document mustafa_mustafa_resume.pdf https://github.com/MustafaMustafa/curriculum_vitae/raw/master/mustafa_mustafa_resume.pdf
 
 git add --all . 
 git commit -m "publish latest CV"
